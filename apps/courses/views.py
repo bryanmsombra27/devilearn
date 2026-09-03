0a287add21f158b49497e25bf3051268fff8959b
bryanmsombra27 <bryanjm96@gmail.com>
2026-09-03 16:59:11 -0600

    adding templates

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+courses = [
+    {
+        "id": 1,
+        "level": "principante",
+        "rating": 4.8,
+        "course_title": "Python fundamentos hasta los detalles",
+        "instructor": "Alison Doe",
+        "course_image": "images/curso_1.jpg",
+        "instructor_image": "https://randomuser.me/api/portraits/women/68.jpg"
+    }
+]
 
 
 def course_list(request):
-    courses = [
-        {
-            "id": 1,
-            "level": "principante",
-            "rating": 4.8,
-            "course_title": "Python fundamentos hasta los detalles",
-            "instructor": "Alison Doe",
-            "course_image": "images/curso_1.jpg",
-            "instructor_image": "https://randomuser.me/api/portraits/women/68.jpg"
-        }
-    ]
 
     return render(request, 'courses/courses.html', {
         "courses": courses
@@ -22,8 +22,64 @@ def course_list(request):
 
 
 def course_detail(request, id):
-    return render(request, 'courses/course-detail.html')
+    # course = [course.id == id for course in courses][0]
+    course = {
+        "course_title": "Django aplicaciones",
+        "course_link": "course_lessons",
+        "course_image": "images/curso_2.jpg",
+        "info_course": {
+            "lessons": 79,
+            "duration": 8,
+            "instructor": "Bryan Ochoa"
+        },
+        "course_content": [
+            {
+                "id": 1,
+                "name": "Introduccion al curso",
+                "lessons": [
+                    {
+                        "name": "¿Que obtentdrás de este curso?",
+                        "type": "video"
+                    },
+                    {
+                        "name": "¿Como usar la plataforma?",
+                        "type": "article"
+                    },
+                ]
+            }
+        ]
+    }
+
+    return render(request, 'courses/course-detail.html', {
+        "course": course
+    })
 
 
 def course_lessons(request, id):
-    return render(request, 'courses/index.html')
+    lesson = {
+        "course_title": "Django aplicaciones",
+        "progress": 30,
+
+        "course_content": [
+            {
+                "id": 1,
+                "name": "Introduccion al curso",
+                "total_lessons": 6,
+                "complete_lessons": 3,
+                "lessons": [
+                        {
+                            "name": "¿Que obtentdrás de este curso?",
+                            "type": "video"
+                        },
+                    {
+                            "name": "¿Como usar la plataforma?",
+                            "type": "article"
+                    },
+                ]
+            }
+        ]
+    }
+
+    return render(request, 'courses/course-lessons.html', {
+        "lesson": lesson
+    })
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+# apps\profiles\views.py
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'profiles/profile.html')
