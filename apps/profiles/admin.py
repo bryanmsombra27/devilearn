beb82e920c7baa1e431272b1a7fdb5fe1129b950
bryanmsombra27 <bryanjm96@gmail.com>
2026-09-16 15:24:14 -0600

    working with models definition

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import InstructorProfile, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + \
+        (("Rol personalizado", {"fields": ("is_instructor",)}),)
+    add_fieldsets = BaseUserAdmin.add_fieldsets + \
+        ((None, {"fields": ("is_instructor",)}),)
+
 
 # Register your models here.
+admin.site.register(InstructorProfile)
--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from .models.user import User
+from .models.instructor import InstructorProfile
+
 
 # Create your models here.
